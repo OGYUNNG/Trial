39df147bdfe6173db423bec152f91d625a2348b6
OGYUNG <olukuogaga5@gmail.com>
2025-07-30 16:08:44 +0100

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 // Entry point for Render deployment
 // This file simply starts the main server
+const path = require('path');
+
+// Ensure we're in the right directory
+console.log('Starting FrosstBank Backend...');
+console.log('Current directory:', __dirname);
+console.log('Server file path:', path.join(__dirname, 'server.js'));
+
+// Start the server
 require('./server.js'); 
